af2c550a2b976c2e9f74446c23978c339d9cfa84
Sean Vickers <seanvickers97@gmail.com>
2021-08-28 18:41:56 -0400

    Added info

---
+++ src/utils/Foo.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class Foo {
+
+	public static void main(String[] args) {
+		System.out.println("howdy");
+		System.out.println("howdy from GitHub");
+		System.out.println("howdy sean1 branch, from Eclipse");
+		System.out.println("hello worlds");
+		System.out.println("Yo from person2");
+	}
+
+}

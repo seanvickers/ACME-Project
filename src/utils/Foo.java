51def53502a294e2227b193e0c27e80ca7266c95
Sean Vickers <seanvickers97@gmail.com>
2021-08-28 16:00:28 -0400

    Added a sean1 branch howdy

--- src/utils/Foo.java
+++ src/utils/Foo.java
@@ -5,6 +5,7 @@ public class Foo {
 	public static void main(String[] args) {
 		System.out.println("howdy");
 		System.out.println("howdy from GitHub");
+		System.out.println("howdy sean1 branch, from Eclipse");
 	}
 
 }
